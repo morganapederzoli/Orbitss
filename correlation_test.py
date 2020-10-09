278b4d87334428f1ab80605eca964e6d160f10da
morganapederzoli <70065980+morganapederzoli@users.noreply.github.com>
2020-10-09 17:37:09 +0200

    Update correlation_test.py

--- correlation_test.py
+++ correlation_test.py
@@ -3,16 +3,14 @@ from numpy import genfromtxt, mean
 from astropy.stats import sigma_clip
 from matplotlib.pylab import *
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.stats import linregress
 
 #funzioni da definire
 
-def dist( _R, _z, _phi, _vR, _vz, _vphi, _i):
-    w = np.zeros(len(_R))
-    for j in range(len(_R)):
-        w[j]=(sqrt((_R[j]-_R[_i])**2+(_z[j]-_z[_i])**2+(_phi[j]-_phi[_i])**2+(_vR[j]-_vR[_i])**2+(_vz[j]-_vz[_i])**2+(_vphi[j]-_vphi[_i])**2))
-        #w[j]=(sqrt((_R[j]-_R[_i])**2+(_z[j]-_z[_i])**2+(_vR[j]-_vR[_i])**2+(_vz[j]-_vz[_i])**2+(_vphi[j]-_vphi[_i])**2))
-    return w
+def dist2( _R,_vR, _i,_z=np.zeros(370), _phi=np.zeros(370),  _vz=np.zeros(370), _vphi=np.zeros(370)): 
+    w=(sqrt((_R-_R[_i])**2+(_z-_z[_i])**2+(_phi-_phi[_i])**2+(_vR-_vR[_i])**2+(_vz-_vz[_i])**2+(_vphi-_vphi[_i])**2))
+    return w 
+
 
 #presa dati
 
@@ -25,85 +23,29 @@ f, (a,b)=subplots(2,1)
 
 x = R[(fabs(z)<1e-4)&(vz>0)]
 y = vR[(fabs(z)<1e-4)&(vz>0)]
-print x.size
 
-a.plot(x,y,'s')
+a.plot(x,y,'s', label='Surface of Section')
+a.legend(loc='upper right')
 
-step=400 #coincide con i punti considerati
-count=np.zeros(step)
+distances=np.zeros((x.size,x.size)) 
 
-Max = 0
-for i in range(step):
-    w = dist(R,z,phi,vR,vz,vphi,i)
-    if Max<= np.amax(w):
-        Max=np.amax(w)
-    print i
-print Max, 'max'
-    
-hyper = np.linspace(0,int(Max)+1,step)
+for i in range(x.size): distances[i]=dist2(x,y,i)
 
-for i in range (x.size):
+hyper = np.linspace(0,int(np.amax(distances))+1,x.size)
 
-    w = dist(R,z,phi,vR,vz,vphi,i)
-    hyperCopy = np.copy(hyper)
+count=np.zeros(x.size)
 
-    for j in range (x.size):
-        epsilon = np.amax(hyperCopy)
-        for k in range(x.size):
-            if w[k] < epsilon:
+for j in range (x.size):
+    for i in range (x.size):
+        for k in range (x.size):
+            if distances[i][k] < hyper[j]:
                 count[j] += (1.0/float(len(x)**2))
-        hyperCopy=np.delete(hyperCopy,-1)
-
-rev_count=count[::-1]
-
 
 b.set_yscale('log')
 b.set_xscale('log')
-b.plot(hyper, rev_count, 's')
+b.plot(hyper, count, 's', label='C(r) in SoS')
+b.legend(loc='upper left')
 
 
 plt.show()
 plt.close()
-
-
-#spazio dellle fasi intero
-
-step=100 #coincide con i punti considerati
-count=np.zeros(step)
-
-Max = 0
-for i in range(step):
-    w = dist(R,z,phi,vR,vz,vphi,i)
-    if Max<= np.amax(w):
-        Max=np.amax(w)
-    print i
-print Max, 'max'
-    
-hyper = np.linspace(0,int(Max)+1,step)
-
-for i in range(step):
-
-    w = dist(R,z,phi,vR,vz,vphi,i)
-    hyperCopy = np.copy(hyper)
-
-    for j in range(step):
-
-        epsilon = np.amax(hyperCopy)
-
-        for k in range(step):
-
-            if w[k] < epsilon:
-
-                count[j] +=(1.0/float(step**2)) 
-            
-        hyperCopy=np.delete(hyperCopy,-1)
-
-rev_count=count[::-1]
-
-plt.yscale('log')
-plt.xscale('log')
-
-plt.plot(hyper, rev_count, 's')
-
-
-plt.show()
