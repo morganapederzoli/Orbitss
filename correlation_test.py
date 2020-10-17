ca289da465664672cc023ee17dbfe4949833dde9
morganapederzoli <70065980+morganapederzoli@users.noreply.github.com>
2020-10-17 15:47:02 +0200

    Update correlation_test.py
    it's gettin better... and fancier! :3
    ATTENTION: now it is required to have the fancyplot file in the same directory of our program

--- correlation_test.py
+++ correlation_test.py
@@ -1,13 +1,19 @@
 import numpy as np
-from numpy import genfromtxt, mean
-from astropy.stats import sigma_clip
+from numpy import genfromtxt
 from matplotlib.pylab import *
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
+from fancyplot import *
+
+#presa dati
+
+data = genfromtxt('ComplexPlummer0_E_-0.050000_Lz_0.001000.txt', delimiter='	', skip_header=1, usecols=(1,2,3,4,5,6))
+R, z, phi, vR, vz, vphi = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5]
+
+n_data=R.size
 
 #funzioni da definire
 
-def dist2( _R,_vR, _i,_z=np.zeros(17102), _phi=np.zeros(17102),  _vz=np.zeros(17102), _vphi=np.zeros(17102)): 
+def dist2( _R,_vR, _i,_z=np.zeros(n_data), _phi=np.zeros(n_data),  _vz=np.zeros(n_data), _vphi=np.zeros(n_data)): 
     w=(sqrt((_R-_R[_i])**2+(_z-_z[_i])**2+(_phi-_phi[_i])**2+(_vR-_vR[_i])**2+(_vz-_vz[_i])**2+(_vphi-_vphi[_i])**2))
     return w
 
@@ -16,8 +22,10 @@ def min_chi_square(_x,_y):
     #M=number of parameters
     sigma=np.zeros(N)
 
-    for i in range(N):
-        sigma[i]=1 # in generale (y_i-y(x_i))**/(N-M)
+    '''for i in range(N):
+    sigma[i]=1 	in generale (y_i-y(x_i))**/(N-M)'''
+
+    sigma=np.ones(N)
 
     S=sum(1/sigma**2)
     S_x=sum(_x/sigma**2)
@@ -30,24 +38,19 @@ def min_chi_square(_x,_y):
     m=(S*S_xy - S_x*S_y)/delta
 
     print("coeff ang :", m)
+    print("normalization :", q)
     return np.array([m,q])
-
-
-#presa dati
-
-data = genfromtxt('ComplexPlummer0_E_-0.050000_Lz_0.001000.txt', delimiter='	', skip_header=1, usecols=(1,2,3,4,5,6))
-R, z, phi, vR, vz, vphi = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5]
 
 #superficie di sezione
 
 
-distances=np.zeros((R.size,R.size)) 
+distances=np.zeros((n_data,n_data)) 
 
-for i in range(R.size):
+for i in range(n_data):
     distances[i]= dist2(R,vR,i)
 
-distances = distances.reshape(R.size*R.size)
-grid = np.logspace(-3, np.log(np.amax(distances)), 200)
+distances = distances.reshape(n_data*n_data)
+grid = np.logspace(-3, np.log10(np.amax(distances)), 200)
 results = np.histogram(distances, bins=grid)
 
 rr = (results[1][1:] + results[1][:-1])/2.
@@ -57,29 +60,31 @@ cumulative[0] = results[0][0]
 for i in range(1,len(rr),1):
     cumulative[i] = cumulative[i-1]+results[0][i]
 
-param=min_chi_square(log(rr[:130]),log(cumulative[:130])) #point 130 should exclude final plateu
+param=min_chi_square(log10(rr[:130]),log10(cumulative[:130])) #point 130 should exclude final plateu
 
 
+fancylayout()
 
 f, (a,b)=subplots(2,1)
-
-#x = R[(fabs(z)<1e-4)&(vz>0)]
-#y = vR[(fabs(z)<1e-4)&(vz>0)]
-
 
 fnt = {'fontsize':16}
 
 plt.rc('text', usetex=True)
 
+
+a.set_xlabel('R', **fnt)
+a.set_ylabel('vR', **fnt)
 a.plot(R,vR,'s', label='Surface of Section')
 a.legend(loc='upper right')
 
 b.set_xscale('log')
 b.set_yscale('log')
-b.plot(rr,cumulative,'s')
-b.plot(rr, (10**param[1])*(rr**param[0]))
+b.set_xlabel('r', **fnt)
+b.set_ylabel('C(r)', **fnt)
 
-b.set_xlabel('R $\\rho$', **fnt)
+b.plot(rr,cumulative,'s', label='Correlation Integral')
+b.plot(rr, (10**param[1])*(rr**param[0]), label='Linear regression')
+b.legend(loc='upper left')
 
 plt.show() #pirla se non lo metti non mostra svegliati
 plt.close()
